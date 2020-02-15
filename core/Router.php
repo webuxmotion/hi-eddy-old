4438e8fe8d4719a5a3a0a3b23f2d46657ee35bef
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-02-15 17:52:13 +0200

    Router. Part 2. Finish

--- core/Router.php
+++ core/Router.php
@@ -20,13 +20,59 @@ class Router {
 
   public static function dispatch($url) {
     if (self::matchRoute($url)) {
-      echo 'OK';
+      $controller = 'app\controllers\\' . 
+        self::$route['prefix'] . 
+        self::$route['controller'] . 'Controller';
+      if (class_exists($controller)) {
+        $controllerObject = new $controller(self::$route);
+        $action = self::$route['action'] . 'Action';
+        if (method_exists($controllerObject, $action)) {
+          $controllerObject->$action();
+        } else {
+          throw new \Exception("Method <b>{$action}</b> in $controller not found");
+        }
+      } else {
+        throw new \Exception("Controller <b>{$controller}</b> not found");
+      }
     } else {
-      echo 'NO';
+      throw new \Exception('Page not found', 404);
     }
   }
 
   public static function matchRoute($url) {
-    return true;
+    foreach (self::$routes as $pattern => $route) {
+      if (preg_match("#{$pattern}#", $url, $matches)) {
+        foreach ($matches as $k => $v) {
+          if (is_string($k)) {
+            $route[$k] = $v;
+          }
+        }
+        if (empty($route['action'])) {
+          $route['action'] = 'index';
+        }
+        if (!isset($route['prefix'])) {
+          $route['prefix'] = '';
+        } else {
+          $route['prefix'] .= '\\';
+        }
+        $route['controller'] = self::upperCamelCase($route['controller']);
+        $route['action'] = self::lowerCamelCase($route['action']);
+        self::$route = $route;
+
+        return true;
+      }
+    }
+    return false;
+  }
+
+  protected static function upperCamelCase($str) {
+    $str = str_replace('-', ' ', $str);
+    $str = ucwords($str);
+    $str = str_replace(' ', '', $str);
+    return $str;
+  }
+
+  protected static function lowerCamelCase($str) {
+    return lcfirst(self::upperCamelCase($str));
   }
 }
--- public/index.php
+++ public/index.php
@@ -3,7 +3,5 @@
 require_once dirname(__DIR__) . '/config/init.php';
 
 use core\Hi;
-use core\Router;
 
 new Hi();
-debug(Router::getRoutes());
