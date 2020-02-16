4b9ddba78592b276d1f4564a3a0a9e549be07c91
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-02-16 14:49:00 +0200

    Show brands on main page

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,15 +3,16 @@
 namespace app\controllers;
 
 use core\Hi;
-use core\Cache;
 
 class MainController extends AppController {
 
   public function indexAction() {
+    $brands = \R::find('brand', 'LIMIT 3');
     $this->setMeta(
       Hi::$eddy->getProperty('site_name'), 
       'Language Teacher', 
       'Teach language, italian, learn italian, english, learn english'
     );
+    $this->set(compact('brands'));
   }
 }
