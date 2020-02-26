b7a9996fc40f4f4c7a6b86fd2bcf16a03ef3f792
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-02-26 22:37:18 +0200

    Product card. Part 2

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use core\Hi;
+
 class ProductController extends AppController {
 
   public function viewAction() {
@@ -11,6 +13,12 @@ class ProductController extends AppController {
       throw new \Exception("Page not found. Product {$alias} not found", 404);
     }
 
+    $curr = Hi::$eddy->getProperty('currency'); 
+    $cats = Hi::$eddy->getProperty('cats');
+    $price = $curr['symbol_left'] . ' ' . $product->price * $curr['value'] . ' ' . $curr['symbol_right'];
+    $oldPrice = $product->old_price
+      ? ($curr['symbol_left'] . ' ' . $product->old_price * $curr['value'] . ' ' . $curr['symbol_right'])
+      : null;
     // breadcrumbs
 
     // related
@@ -22,6 +30,6 @@ class ProductController extends AppController {
     // modifications
 
     $this->setMeta($product->title, $product->description, $product->keywords);
-    $this->set(compact('product'));
+    $this->set(compact('product', 'curr', 'cats', 'price', 'oldPrice'));
   }
 }
--- app/views/Product/view.php
+++ app/views/Product/view.php
@@ -31,12 +31,10 @@
 							  </ul>
 						</div>
 						
-
-						
 					</div>	
 					<div class="col-md-7 single-top-right">
 						<div class="single-para simpleCart_shelfItem">
-						<h2>Lorem Ipsum</h2>
+						<h2><?=$product->title?></h2>
 							<div class="star-on">
 								<ul class="star-footer">
 										<li><a href="#"><i> </i></a></li>
@@ -52,8 +50,13 @@
 							<div class="clearfix"> </div>
 							</div>
 							
-							<h5 class="item_price">$ 95.00</h5>
-							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+							<h5 class="item_price"><?=$price?></h5>
+
+              <?php if ($oldPrice) : ?>
+                <small><del><?=$oldPrice?></del></small>
+              <?php endif; ?>
+
+              <?=$product->content;?>
 							<div class="available">
 								<ul>
 									<li>Color
@@ -74,12 +77,17 @@
 							</ul>
 						</div>
 							<ul class="tag-men">
-								<li><span>TAG</span>
-								<span class="women1">: Women,</span></li>
-								<li><span>SKU</span>
-								<span class="women1">: CK09</span></li>
+								<li><span>Category</span>
+								<span class="women1">: 
+                  <a href="/category/<?=$cats[$product->category_id]['alias']?>"><?=$cats[$product->category_id]['title']?></a>
+                </span></li>
 							</ul>
-								<a href="#" class="add-cart item_add">ADD TO CART</a>
+              <input type="number" size="4" value="1" name="quantity" min="1" step="1">
+								<a 
+                  id="productAdd" 
+                  data-id="<?=$product->id;?>" 
+                  href="/cart/add?id=<?=$product->id;?>" 
+                  class="add-cart item_add add-to-cart-link">ADD TO CART</a>
 							
 						</div>
 					</div>
